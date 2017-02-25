3aa6d3379c0847d9d5d1454883d8158b011b2e1f
s-jcs <shosundberg@gmail.com>
2017-02-25 18:24:10 +0900

    add markerer to map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,5 @@
 $(function() {
 
-  console.log('foobar')
-
 	var mapOptions = {
 		zoom: 5,
 		center: new google.maps.LatLng(35.652832, 139.839478),
@@ -17,5 +15,29 @@ $(function() {
 	  scaleControl: false
 	};
 
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  var locations = [
+  {lat: 35.6895, lng: 139.6917},
+  {lat: 35.0116, lng: 135.7680},
+  {lat: 38.2682, lng: 140.8694},
+  {lat: 37.7503, lng: 140.4676},
+  {lat: 31.9077, lng: 131.4202},
+  {lat: 32.3898, lng: 130.7417},
+  {lat: 32.7503, lng: 129.8777},
+  {lat: 33.5904, lng: 130.4017}
+  ]
+
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var markers = locations.map(function(location, i) {
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length]
+      });
+  });
+
+  var markerCluster = new MarkerClusterer(map, markers,
+  {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
 });
